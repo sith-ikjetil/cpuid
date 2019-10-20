535ffde52133ad83af0b831af8e8669fb2cc2e2b
sith-ikjetil <post@ikjetil.no>
2019-10-20 06:55:30 +0200

    transformed to lowercase

--- cpuid_linux.asm
+++ cpuid_linux.asm
@@ -1,22 +1,22 @@
-        GLOBAL          _start
+        global          _start
         
-        SECTION         .data
-msg:    DB              "123456789012", 10
+        section         .data
+msg:    db              "123456789012", 10
         
-        SECTION         .text
+        section         .text
 _start:        
-        XOR             EAX, EAX
-        CPUID                               ; get cpu ident
-        MOV             [msg], EBX          ; Genu
-        MOV             [msg+4], EDX        ; ineI
-        MOV             [msg+8], ECX        ; ntel
+        xor             eax, eax
+        cpuid                               ; get cpu ident
+        mov             [msg], ebx          ; Genu
+        mov             [msg+4], edx        ; ineI
+        mov             [msg+8], ecx        ; ntel
 
-        MOV             RAX, 1              ; system call for write
-        MOV             RDI, 1              ; stdout
-        MOV             RSI, msg            ; address of message to write
-        MOV             RDX, 13             ; number of bytes
-        SYSCALL                             ; invoke
+        mov             rax, 1              ; system call for write
+        mov             rdi, 1              ; stdout
+        mov             rsi, msg            ; address of message to write
+        mov             rdx, 13             ; number of bytes
+        syscall                             ; invoke
 
-        MOV             RAX, 60             ; system call for exit
-        XOR             RDI, RDI            ; exit code 0
-        SYSCALL                             ; invoke
+        mov             rax, 60             ; system call for exit
+        xor             rdi, rdi            ; exit code 0
+        syscall                             ; invoke
